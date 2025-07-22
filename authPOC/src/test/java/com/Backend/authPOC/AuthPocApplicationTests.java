210815cf5aa011a853c069d45af48a3bf430001f
Divyam-12 <patnidivyam@gmail.com>
2025-07-22 15:56:47 +0530

    Initial commit

---
+++ authPOC/src/test/java/com/Backend/authPOC/AuthPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend.authPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
