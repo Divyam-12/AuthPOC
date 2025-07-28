d96a115c20052446eb4f12321798419df2b9498c
Divyam-12 <patnidivyam@gmail.com>
2025-07-28 22:07:05 +0530

    Update AuthenticationResponse

--- backend/src/main/java/com/JWT/auth_api/model/AuthenticationResponse.java
+++ backend/src/main/java/com/JWT/auth_api/model/AuthenticationResponse.java
@@ -1,36 +1,15 @@
 package com.JWT.auth_api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationResponse {
-	private String token;
-	//new
-	private String role;
-	//new
-	
-	//old
-//	public AuthenticationResponse(String token)
-//	{
-//		this.token=token;
-//	}
-	
-	//new
-	public AuthenticationResponse(String token, String role) {
-	    this.token = token;
-	    this.role = role;
-	}
-	//new
-	
-	public String getToken()
-	{
-		return token;
-	}
-
-	//new
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-	//new
+    private String token;
+    private String role;
 }
