883374a5813ab497cc0bc0421a110c32b4275894
Divyam-12 <patnidivyam@gmail.com>
2025-08-05 23:04:06 +0530

    Cosine Similarity increased

--- frontend/src/Components/protectedPage.jsx
+++ frontend/src/Components/protectedPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { ping } from "../Services/api";
+import { ping } from "../services/api";
 import { AuthContext } from "../contexts/AuthContext";
 
 export default function ProtectedPage() {
